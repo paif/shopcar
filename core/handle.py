d225a59d4d55cc509e77ce26b7e72146dd60a0ae
paif <52371126+paif@users.noreply.github.com>
2020-08-12 17:00:32 +0800

    Update handle.py

--- core/handle.py
+++ core/handle.py
@@ -1,62 +1,37 @@
-from scripts.parse_paramters import Paramter
-from scripts.utils import discount
-
-all_goods = {
-    "电子": ["ipad", "iphone", "显示器", "笔记本电脑", "键盘"],
-    "食品": ["面包", "饼干", "蛋糕", "牛肉", "鱼", "蔬菜"],
-    "日用品": ["餐巾纸", "收纳箱", "咖啡杯", "雨伞"],
-    "酒类": ["啤酒", "白酒", "伏特加"]
-}
-def discount(category, rate):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            if g_name in all_goods.get(category):
-                result = func(g_name, g_num, g_price * rate)
-
-            else:
-                result = func(g_name, g_num, g_price)
-            return result
-        return wrapper
-    return decorator
+from scripts.utils import parse_date, parse_goods_list
+from date.goods import all_goods
 
 
-
-@discount(category, rate)
-def one_goods_price(g_name, g_num, g_price):
-    """
-    一种商品的总价格
-    :param g_name:商品名称
-    :param g_num: 商品数量
-    :param g_price: 商品价格
-    :return: {g_name:g_price}
-    """
-    return {g_name: g_num * g_price}
-
-
-if __name__ == '__main__':
-    s = """
-    2013.11.11 | 0.7 | 电⼦
-    2013.12.22 | 0.8 | 食品
-
-    1 * ipad : 2399.00
-    1 * 显示器 : 1799.00
-    12 * 啤酒 : 26.00
-    5 * 面包 : 9.00
-
-    2013.11.11
-    2014.3.2 1000 200
-
-    """
-    p = Paramter(s)
-    promotions = p.parse_promotion()
-    if promotions:
-        # 有促销
-
-        for good in p.parse_good():
-            g_num, g_name, g_price = good
+class Handle:
+    @staticmethod
+    def promotion(promotions, goods_infos, deadline):
+        if promotions:
+            deadline, parse_date(deadline)
             for promotion in promotions:
                 date, rate, category = promotion
-    else:
-        # 没有促销
-
+                date = parse_date(date)
+                if deadline <= date:
+                    # 结算日期在促销日期里
+                    i = 0
+                    while i < len(goods_infos):
+                        if goods_infos[i][1] in all_goods.get(category):
+                            goods_infos[i][2] = goods_infos[i][2] * rate
+                        i += 1
+                else:
+                    # 结算日期超过促销日期
+                    pass
+                
+        return parse_goods_lists(goods_infos)
+    
+    @staticmethod
+    def coupons(deadline, coup, total_price):
+        if coup:
+            # 如果有优惠券
+            date, base_price, derates = coup.strip().split(" ")
+            if total_price > float(base_price):
+                if parse_date(deadline) <= parse_date(date):
+                    total_price = total_price -float(derates)
+        return "%.2f" % total_price
+    
+    
 
