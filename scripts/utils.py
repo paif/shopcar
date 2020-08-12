dd356556d374ad5843cfaee72e485b06fb80e5b8
paif <52371126+paif@users.noreply.github.com>
2020-08-12 16:42:10 +0800

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,6 +4,11 @@ def parse_date(date_string):
     year, month, day = date_string.split(".")
     return datetime.date(int(year), int(month), int(day))
 
+def parse_goods_list(goods_list):
+    result = {}
+    for _ in goods_list:
+        result[_[1]] = float("%.2f" % (_[0]*_[2]))
+    return result
 
 if __name__ == '__main__':
     print(parse_date("2013.11.12"))
